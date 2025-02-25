cf188d8dc47e0e835e67c7a30d0946fdd31ed978
nn <townhouse.owner@gmail.com>
2025-02-25 15:19:59 +0700

    change port

--- traffic_monitor.py
+++ traffic_monitor.py
@@ -154,4 +154,4 @@ def get_data():
 threading.Thread(target=fetch_traffic_data, daemon=True).start()
 
 if __name__ == '__main__':
-    app.run(debug=True, host="127.0.0.1", port=5002)
+    app.run(debug=True, host="0.0.0.0", port=80)
